fa9be16564a4302e8e060d435efa9c97ae0abb84
Muhammad Sulthan Angka Kurniawan <45189140+sulthankurniawan@users.noreply.github.com>
2021-05-03 19:50:40 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -32,23 +32,26 @@ Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy'])->name('pizzas
 // ]);
 
 // HOME ROUTES
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); // GET HOMEPAGE DUMMY
+Route::get('/home-company', [App\Http\Controllers\HomeController::class, 'homeCompany'])->name('home')->middleware('company'); // GET HOMEPAGE COMPANY (AUTH AS COMPANY)
+Route::get('/home-seeker', [App\Http\Controllers\HomeController::class, 'homeSeeker'])->name('home')->middleware('seeker'); // GET HOMEPAGE SEEKER (AUTH AS SEEKER)
+Route::get('/home-admin', [App\Http\Controllers\HomeController::class, 'homeAdmin'])->name('home')->middleware('admin'); // GET HOMEPAGE ADMIN (AUTH AS ADMIN)
 
 // JOB ROUTES
-Route::get('/job', [App\Http\Controllers\JobController::class, 'index'])->name('job.index');
-Route::get('/job/create', [App\Http\Controllers\JobController::class, 'create'])->name('job.create')->middleware('company');
-Route::get('/job/{id}', [App\Http\Controllers\JobController::class, 'show'])->name('job.show')->middleware('company');
-Route::post('/job', [App\Http\Controllers\JobController::class, 'store'])->name('job.store')->middleware('company');
-Route::delete('/job/{id}', [App\Http\Controllers\JobController::class, 'destroy'])->name('job.destroy')->middleware('company');
+Route::get('/jobs', [App\Http\Controllers\JobController::class, 'index'])->name('jobs.index'); // GET ALL JOB RECORD
+Route::get('/jobs/create', [App\Http\Controllers\JobController::class, 'create'])->name('jobs.create')->middleware('company'); // GET FORM CREATE JOB (AUTH AS COMPANY)
+Route::get('/jobs/{id}', [App\Http\Controllers\JobController::class, 'show'])->name('jobs.show')->middleware('company'); // GET JOB BASED ON ID (AUTH AS COMPANY)
+Route::post('/jobs', [App\Http\Controllers\JobController::class, 'store'])->name('jobs.store')->middleware('company'); // POST JOB FORM (AUTH AS COMPANY)
+Route::delete('/jobs/{id}', [App\Http\Controllers\JobController::class, 'destroy'])->name('jobs.destroy')->middleware('company'); // DELETE JOB RECORD BASED ON ID (AUTH AS COMPANY)
 
 // REQUEST ROUTES
-Route::get('/request', [App\Http\Controllers\RegisterController::class, 'index'])->name('register.index');
-Route::get('/request/create', [App\Http\Controllers\RegisterController::class, 'create'])->name('register.create');
-Route::get('/request/{id}', [App\Http\Controllers\RegisterController::class, 'show'])->name('register.show');
-Route::post('/request', [App\Http\Controllers\RegisterController::class, 'store'])->name('register.store');
-Route::delete('/request/{id}', [App\Http\Controllers\RegisterController::class, 'destroy'])->name('register.destroy');
+Route::get('/requests', [App\Http\Controllers\RequestController::class, 'index'])->name('requests.index'); // GET ALL REQUEST RECORD
+Route::get('/requests/create', [App\Http\Controllers\RequestController::class, 'create'])->name('requests.create')->middleware('seeker'); // GET FORM CREATE REQUEST (AUTH AS SEEKER)
+Route::get('/requests/{id}', [App\Http\Controllers\RequestController::class, 'show'])->name('requests.show')->middleware('auth'); // GET REQUEST BASED ON ID (AUTH)
+Route::post('/requests', [App\Http\Controllers\RequestController::class, 'store'])->name('requests.store')->middleware('seeker'); // POST REQUEST FORM (AUTH AS SEEKER)
+Route::delete('/requests/{id}', [App\Http\Controllers\RequestController::class, 'destroy'])->name('requests.destroy')->middleware('admin'); // DELETE REQUEST BASED ON ID (AUTH AS ADMIN)
 
 // USER ROUTES
-Route::get('/user', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('job.index');
-Route::get('/user/{id}', [App\Http\Controllers\Auth\RegisterController::class, 'show'])->name('job.show');
-Route::delete('/user/{id}', [App\Http\Controllers\Auth\RegisterController::class, 'destroy'])->name('job.destroy');
+Route::get('/users', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('users.index')->middleware('admin'); // GET ALL USER RECORD (AUTH AS ADMIN)
+Route::get('/users/{id}', [App\Http\Controllers\Auth\RegisterController::class, 'show'])->name('users.show')->middleware('auth'); // GET USER BASED ON ID (AUTH)
+Route::delete('/users/{id}', [App\Http\Controllers\Auth\RegisterController::class, 'destroy'])->name('users.destroy')->middleware('admin'); // DELETE USER BASED ON ID (AUTH AS ADMIN)
