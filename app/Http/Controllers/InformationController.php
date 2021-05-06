687faf06220c3a0099a357294ec2b643d9e3e0d8
Muhammad Sulthan Angka Kurniawan <45189140+sulthankurniawan@users.noreply.github.com>
2021-05-07 01:39:29 +0700

    controller

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -6,5 +6,45 @@ use Illuminate\Http\Request;
 
 class InformationController extends Controller
 {
-    //
+    public function index() {
+        // get data from db
+
+        $informations = Information::all();
+
+        return view('informations.index', [
+            'informations' => $informations,
+        ]);
+    }
+
+    public function show($id) {
+        // use the $id variable to query the db for a record
+
+        $information = Information::findOrFail($id);
+
+        return view('informations.show', ['information' => $information]);
+    }
+
+    public function create() {
+        return view('informations.create');
+    }
+
+    public function store() {
+
+        $information = new Information();
+
+        $information->id_user = request('title');
+        $information->alamat = request('content');
+        $information->posisi_loker = request('image');
+
+        $information->save();
+
+        return redirect('/')->with('mssg', 'Informasi telah tersimpan');
+    }
+
+    public function destroy($id) {
+        $information = Information::findOrFail($id);
+        $information->delete();
+
+        return redirect('/');
+    }
 }
--- database/migrations/2021_05_06_172528_create_information_table.php
+++ database/migrations/2021_05_06_172528_create_information_table.php
@@ -16,7 +16,9 @@ class CreateInformationTable extends Migration
         Schema::create('information', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string
+            $table->string('title');
+            $table->string('content');
+            $table->string('image');
         });
     }
 
