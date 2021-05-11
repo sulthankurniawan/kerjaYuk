3c2ad69e8b65f7be9ee5123add9c3cd607756457
Arya Wahyu <arya.wahyu502@gmail.com>
2021-05-11 23:11:24 +0700

    Pemecahan komponen2 seperti footer, navbar dsb ke file blade

--- resources/views/dashboard/seeker.blade.php
+++ resources/views/dashboard/seeker.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.guest')
 
 @section('title')
 KerjaYuk | Home
---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,5 @@
+<script src="{{ url("kerjayuk-template/libraries/jquery/jquery-3.6.0.min.js") }}"></script>
+<script src="{{ url("kerjayuk-template/libraries/bootstrap/js/bootstrap.js") }}"></script>
+<script src="{{ url("kerjayuk-template/libraries/owl-carousel/js/owl.carousel.min.js") }}"></script>
+<script src="{{ url("kerjayuk-template/libraries/aos/js/aos.js") }}"></script>
+<script src="{{ url("kerjayuk-template/scripts/main.js") }}"></script>
