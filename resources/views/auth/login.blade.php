b45a55a5f134c81ca9adf2c14165af93660b2608
Muhammad Sulthan Angka Kurniawan <45189140+sulthankurniawan@users.noreply.github.com>
2021-05-29 17:57:45 +0700

    rapihin view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,56 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
+@section('title')
+KerjaYuk | Login
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<!-- Main -->
+<main>
+    <section class="section-login">
+        <div class="container">
+            <div class="col-md-4 offset-md-4 px-0">
+                <div class="card">
+                    <div class="card-header bg-transparent">
+                        <h3 class="text-left">Masuk</h3>
+                        <p class="text-inactive">Sangat mudah dan cepat. Kami tidak akan mengunggah apapun tanpa
+                            izin
+                            dari anda</p>
+                    </div>
+                    <form action="" method="post">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for=""><strong>Email</strong></label>
+                                <input type="text" name="email" class="form-control" placeholder="Email">
                             </div>
+                            <div class="form-group">
+                                <label for=""><strong>Password</strong></label>
+                                <input type="password" name="password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="form-check mb-3">
+                                <label class="form-check-label">
+                                    <p><label for="checkbox"><input type="checkbox" name="setcookie" value="true"
+                                                id="remember" /> Remember Me</label></p>
+                                </label>
+                            </div>
+                            <a href="#">
+                                <legend id="forgot-pass">Lupa Kata Sandi?</legend>
+                            </a>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="card-footer">
+                            <button type="submit"
+                                class="btn btn-red text-white primary btn-block d-lg-center-flex mb-2 mb-md-0">Masuk</button>
+                            <p class="text-center">Pengguna baru? <a href="#">Buat akun</a>
+                                sekarang!</p>
+                            <p class="text-inactive">Dengan memilih "Masuk" saya telah membaca dan menyetujui
+                                persyaratan penggunaan KerjaYuk.com dan kebijakan privasi</p>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+</main>
+<!-- End Main -->
 @endsection
